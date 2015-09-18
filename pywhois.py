2d41306c1825153629384e0ad25c80d0c5b47af1
luodichen <me@luodichen.com>
2015-09-19 01:44:15 +0800

    bugs fixed

--- pywhois.py
+++ pywhois.py
@@ -44,6 +44,17 @@ class ServerList(object):
         return ret
 
 class PyWhois(object):
+    MULTI_RECORD_PATT = 'To single out one record, look it up with "xxx", ' + \
+                        'where xxx is one of the\\s+records displayed above\\. ' + \
+                        'If the records are the same, look them up\\s+' + \
+                        'with "=xxx" to receive a full display for each record\\.'
+
+    REDIRECT_PATT = r'Domain names in the \.com and \.net domains can now ' + \
+                    r'be registered\s+with many different competing ' + \
+                    r'registrars\. Go to http://www\.internic\.net\s+for ' + \
+                    r'detailed information\..*\s*Domain Name:.*\s+' + \
+                    r'Whois Server:\s+(.+?)\s+'
+
     def __init__(self, server_list=None):
         if server_list is None:
             self.servers = ServerList()
@@ -51,21 +62,21 @@ class PyWhois(object):
             self.servers = ServerList(server_list)
             
     def query(self, domain_name, server):
-        ret = None
-        while server is not None:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((server, 43, ))
-            s.send(domain_name + '\r\n')
-            
-            result = ''
-            while True:
-                buf = s.recv(1024)
-                if len(buf) > 0:
-                    result += buf
-                else:
-                    s.close()
-                    break
-            
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((server, 43, ))
+        s.send(domain_name + '\r\n')
+        
+        result = ''
+        while True:
+            buf = s.recv(1024)
+            if len(buf) > 0:
+                result += buf
+            else:
+                s.close()
+                break
+        
+        return result
+    '''
             ret = result
             redirect = result.find('Domain names in the .com and .net domains '
                                    + 'can now be registered\nwith many different '
@@ -77,10 +88,27 @@ class PyWhois(object):
             server = None if match is None or redirect == -1 else match.groups()[0]
         
         return ret
-    
+    '''
+   
     def getwhois(self, domain_name):
+        original_domain_name = domain_name
         server = self.servers.whois_server(domain_name)
         if server is None:
             raise WhoisServerNotFoundError()
 
-        return self.query(domain_name, server)
+        ret = None
+        
+        while True:
+            response = self.query(domain_name, server)
+            multi_record = re.search(self.MULTI_RECORD_PATT, response)
+            redirect = re.search(self.REDIRECT_PATT, response, re.S)
+            if multi_record:
+                domain_name = '=' + original_domain_name
+            elif redirect:
+                domain_name = original_domain_name
+                server = redirect.groups()[0]
+            else:
+                ret = response
+                break
+        
+        return ret
